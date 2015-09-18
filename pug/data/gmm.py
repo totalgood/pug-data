257cc0cc2e2adcb872a9089e5a81c8912be60e1a
Hobson Lane <hobsonlane@gmail.com>
2015-09-17 22:50:03 -0700

    broken gmm

--- pug/data/gmm.py
+++ pug/data/gmm.py
@@ -1,8 +1,11 @@
-"""Generate synthetic data from a Gaussian Mixture Model (GMM)
+"""Generate synthetic samples from a mixture of gaussians or Gaussian Mixture Model (GMM)
 
-References:
+Most of this code is derived from Nehalem Labs examples:
   http://www.nehalemlabs.net/prototype/blog/2014/04/03/quick-introduction-to-gaussian-mixture-models-with-python/
 
+References:
+  https://en.wikipedia.org/wiki/Metropolis%E2%80%93Hastings_algorithm
+  https://en.wikipedia.org/wiki/Mixture_model#Gaussian_mixture_model
 """
 
 import numpy as np
@@ -12,19 +15,33 @@ from mpl_toolkits.mplot3d import Axes3D
 from sklearn import mixture
 
 
-def gaussians(x, y):
-    g1 = mlab.bivariate_normal(x, y, 1.0, 1.0, -1, -1, -0.8)
-    g2 = mlab.bivariate_normal(x, y, 1.5, 0.8, 1, 2, 0.6)
-    return 0.6 * g1 + 28.4 * g2 / (0.6 + 28.4)
+def gaussian(x=None, y=None, sigma=(1., 1.), mu=(-1, -1), sigmaxy=-.75):
+    try:
+        x = float(x)
+    except:
+        x = np.arange(-5 * sigma[0], 5 * sigma[0], .2 * sigma[0])
+    try:
+        y = float(y)
+    except:
+        y = np.arange(-5 * sigma[1], 5 * sigma[1], .2 * sigma[1])
+    X, Y = np.meshgrid(x, y)
+    if X.shape == (1, 1):
+        X = X[0, 0]
+    if Y.shape == (1, 1):
+        Y = Y[0, 0]
+    return mlab.bivariate_normal(X, Y, sigma[0], sigma[1], mu[0], mu[1], sigmaxy)
 
 
-def plot_gaussians():
+def gaussian_mix(N=2, ratio=.7):
+    p1 = gaussian()
+    p2 = gaussian(sigma=(1.6, .8), mu=(.8, 0.5))
+    return ratio * p1 + (30. - ratio) * p2 / ratio
+    # return gaussian() + gaussian()
+
+
+def plot_gaussians(X=None, Y=None, Z=None):
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    X = np.arange(-5, 5, 0.1)
-    Y = np.arange(-5, 5, 0.1)
-    X, Y = np.meshgrid(X, Y)
-    Z = gaussians(X, Y)
     surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=plt.get_cmap('coolwarm'),
                            linewidth=0, antialiased=True)
     fig.colorbar(surf, shrink=0.5, aspect=5)
@@ -43,13 +60,14 @@ def sample_prgs(N=10000, s=10, prgs=[np.random.normal] * 2, show=True, save='prg
         default: np.random.normal
     '''
     r = np.zeros(2)
-    p = q(r[0], r[1])
+    p = gaussian()
     if verbosity > 0:
         print("True mean: {}".format(p))
     samples = []
     for i in xrange(N):
         rn = r + np.array([prg(size=1) for prg in prgs]).T[0]
-        pn = q(rn[0], rn[1])
+        # get a single probability from the mixed distribution
+        pn = gaussian_mix(rn[0], rn[1])
         if pn >= p:
             p = pn
             r = rn
@@ -70,7 +88,7 @@ def sample_prgs(N=10000, s=10, prgs=[np.random.normal] * 2, show=True, save='prg
     x = np.arange(np.min(samples), np.max(samples), dx)
     y = np.arange(np.min(samples), np.max(samples), dx)
     X, Y = np.meshgrid(x, y)
-    Z = q(X, Y)
+    Z = gaussian_mix(X, Y)
     CS = plt.contour(X, Y, Z, 10, alpha=0.5)
     plt.clabel(CS, inline=1, fontsize=10)
     if save:
@@ -93,6 +111,6 @@ def fit_gmm(samples, show=True, save='class_predictions.png', verbosity=1):
 
 
 if __name__ == '__main__':
-    plot_q()
+    plot_gaussians()
     samples = sample_prgs()
     fit_gmm(samples)
