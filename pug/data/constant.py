9442909c31e2a98aec2eb29aacdae7692d8a9019
Hobson Lane <github@totalgood.com>
2016-06-05 18:18:45 -0700

    add more roman nums and tests

--- pug/data/constant.py
+++ pug/data/constant.py
@@ -1,9 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Constants (paths, translation dictionaries)
+>>> os.path.isdir(DATA_PATH)
+True
+>>> DICT_ROMAN2INT['IX']
+9
+>>> DICT_ROMAN2INT['XLII']
+42
+>>> DICT_ROMAN2INT['XXX']
+30
+>>> DICT_ROMAN2INT['LXXV']
+85
+"""
+
 import os
 
 MODULE_PATH = os.path.abspath(os.path.dirname(__file__))
 DATA_PATH = os.path.join(MODULE_PATH, 'cache')
-DICT_ROMAN2INT = {'I': 1, 'II': 2, 'III': 3, 'IV': 4,  'V': 5,
-                  'VI': 6, 'VII': 7, 'VIII': 8, 'IX': 9, 'X': 10}
-for num_X in range(1, 5):
-    for s, num in DICT_ROMAN2INT.items():
-        DICT_ROMAN2INT['X' * num_X + s] = 10 + num
+DICT_ROMAN2INT = R2I_10 = {'': 0, 'I': 1, 'II': 2, 'III': 3, 'IV': 4,  'V': 5,
+                           'VI': 6, 'VII': 7, 'VIII': 8, 'IX': 9}
+for num_X in range(1, 4):
+    for s, num in R2I_10.items():
+        DICT_ROMAN2INT['X' * num_X + s] = 10 * num_X + num
+for (prefix, value) in [('XL', 40), ('L', 50)]:
+    DICT_ROMAN2INT.update(dict([(prefix + s, value + num) for s, num in R2I_10]))
+for num_X in range(1, 4):
+    DICT_ROMAN2INT.update(dict([('L' + 'X' * num_X, 50 + 10 * num_X + num) for s, num in R2I_10]))
---
+++ tests/test_constant.py
@@ -0,0 +1,22 @@
+"""Run doctests in pug.data.constant"""
+
+import unittest
+import doctest
+
+import pug.constant
+
+
+class T(unittest.TestCase):
+    """Do-Nothing Test to ensure unittest doesnt ignore this file"""
+
+    def setUp(self):
+        pass
+
+    def test_truth(self):
+        self.assertTrue(True)
+
+
+def load_tests(loader, tests, ignore):
+    """Run doctests for the clayton.nlp module"""
+    tests.addTests(doctest.DocTestSuite(pug.data.constant, optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE))
+    return tests
