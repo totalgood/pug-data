5e13d76f0c4659d314f49f5b1c40f40f9b0cc171
Hobson Lane <github@totalgood.com>
2016-06-05 19:10:00 -0700

    add doctests for Shakespeare corpus

--- pug/data/corpus.py
+++ pug/data/corpus.py
@@ -21,7 +21,16 @@ RE_BY_LINE = re.compile(r'^((by\ )?(William\ Shakespeare))$', re.IGNORECASE)
 def generate_lines(path=PATH_SHAKESPEARE,
                    start=0,
                    stop=float('inf')):
-    """Generate (yield) lines in a gzipped file (*.txt.gz) one line at a time"""
+    r"""Generate (yield) lines in a gzipped file (*.txt.gz) one line at a time
+
+    >>> i = 0
+    >>> for i, line in enumerate(generate_lines()):
+    ...     pass
+    >>> i
+    124786
+    >>> iter(generate_lines()).next()
+    '\xef\xbb\xbfThe Project Gutenberg EBook of The Complete Works of William Shakespeare, by'
+    """
     with (gzip.open(path, 'rU') if path.endswith('.gz') else open(path, 'rU')) as f:
         for i, line in enumerate(f):
             if i < start:
@@ -34,6 +43,24 @@ def generate_lines(path=PATH_SHAKESPEARE,
 def segment_shakespeare_works(path=PATH_SHAKESPEARE, verbose=False):
     """Find start and end of each volume within _Complete Works of William Shakespeare_
 
+    >>> segment_shakespeare_works()
+    {'body_start': 21,
+     'title': 'COMPLETE WORKS--WILLIAM SHAKESPEARE',
+     'volumes': [{'by': 'William Shakespeare',
+       'by_lineno': 177,
+       'start': 173,
+       'stop': 2798,
+       'title': 'THE SONNETS',
+       'title_lineno': 175,
+       'year': 1609},
+    ...
+    {'by': 'William Shakespeare',
+     'by_lineno': 123986,
+     'start': 123982,
+     'stop': 124366,
+     'title': "A LOVER'S COMPLAINT",
+     'title_lineno': 123984,
+     'year': 1609}]}
     """
     works = [{}]
     meta = {}
---
+++ tests/test_corpus.py
@@ -0,0 +1,22 @@
+"""Run doctests in pug.data.corpus"""
+
+import unittest
+import doctest
+
+import pug.data.corpus
+
+
+class T(unittest.TestCase):
+    """Do-Nothing Test to ensure unittest doesnt ignore this file"""
+
+    def setUp(self):
+        pass
+
+    def test_truth(self):
+        self.assertTrue(True)
+
+
+def load_tests(loader, tests, ignore):
+    """Run doctests for the clayton.nlp module"""
+    tests.addTests(doctest.DocTestSuite(pug.data.corpus, optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE))
+    return tests
