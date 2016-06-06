8eea5662970fd9a298b2cca8fbe145ab05a432fa
Hobson Lane <github@totalgood.com>
2016-06-05 18:48:54 -0700

    tests pass 7pct cov

--- pug/data/constant.py
+++ pug/data/constant.py
@@ -17,12 +17,13 @@ import os
 
 MODULE_PATH = os.path.abspath(os.path.dirname(__file__))
 DATA_PATH = os.path.join(MODULE_PATH, 'cache')
-DICT_ROMAN2INT = R2I_10 = {'': 0, 'I': 1, 'II': 2, 'III': 3, 'IV': 4,  'V': 5,
-                           'VI': 6, 'VII': 7, 'VIII': 8, 'IX': 9}
+DICT_ROMAN2INT = {'': 0, 'I': 1, 'II': 2, 'III': 3, 'IV': 4,  'V': 5,
+                  'VI': 6, 'VII': 7, 'VIII': 8, 'IX': 9}
+R2I_10 = dict(DICT_ROMAN2INT)
 for num_X in range(1, 4):
     for s, num in R2I_10.items():
         DICT_ROMAN2INT['X' * num_X + s] = 10 * num_X + num
 for (prefix, value) in [('XL', 40), ('L', 50)]:
-    DICT_ROMAN2INT.update(dict([(prefix + s, value + num) for s, num in R2I_10]))
+    DICT_ROMAN2INT.update(dict([(prefix + s, value + num) for s, num in R2I_10.items()]))
 for num_X in range(1, 4):
-    DICT_ROMAN2INT.update(dict([('L' + 'X' * num_X, 50 + 10 * num_X + num) for s, num in R2I_10]))
+    DICT_ROMAN2INT.update(dict([('L' + 'X' * num_X, 50 + 10 * num_X + num) for s, num in R2I_10.items()]))
